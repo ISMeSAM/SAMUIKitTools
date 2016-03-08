a91f656d95663ccc58920e714944fc26c5b923c7
sam <wangyongzhen_wyz@163.com>
2016-03-08 09:35:18 +0800

    init

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by samiMac on 16/3/8.
+//  Copyright © 2016年 wydatas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
